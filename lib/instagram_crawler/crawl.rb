e23d9eac4c3d321de19e345b430d728dd289f01f
F <fwrizalman@gmail.com>
2018-01-16 01:04:25 +0800

    Fix broken search with new endpoints

--- lib/instagram_crawler/crawl.rb
+++ lib/instagram_crawler/crawl.rb
@@ -25,8 +25,9 @@ module InstagramCrawler
       parse_page = get_and_parse(url)
       json = get_parsed_json(parse_page)
       data = json.any? ? get_json_nodes(json) : json
+      nodes = get_data_nodes(data)
 
-      mapped_data = data.map do |post|
+      mapped_data = nodes.map do |post|
         get_hash_for_post(post)
       end
       mapped_data
@@ -54,10 +55,19 @@ module InstagramCrawler
 
       def get_json_nodes(json)
         page = is_user ? 'ProfilePage' : 'TagPage'
-        type = is_user ? 'user' : 'tag'
+        type = is_user ? 'user' : 'hashtag'
 
         json['entry_data'][page]
-          .first[type]['media']['nodes'].first(limit)
+          .first['graphql'][type]['edge_hashtag_to_media']['edges']
+          .first(limit)
+      end
+
+      def get_data_nodes(edges)
+        nodes = Array.new
+        edges.each do |edge|
+          nodes << edge['node']
+        end
+        return nodes
       end
 
       def get_hash_for_post(post)
@@ -65,11 +75,11 @@ module InstagramCrawler
           id: post['id'],
           images: {
             thumbnail: { url: post['thumbnail_src'] },
-            standard_resolution: { url: post['display_src'] }
+            standard_resolution: { url: post['display_url'] }
           },
-          code: post['code'], caption: post['caption'],
-          created_time: post['date'], link: LINK_URL_PREFIX + post['code'] + '/',
-          likes: post['likes'], comments: post['comments']
+          code: post['shortcode'], caption: post['edge_media_to_caption'],
+          created_time: post['taken_at_timestamp'], link: LINK_URL_PREFIX + post['shortcode'] + '/',
+          likes: post['edge_liked_by'], comments: post['edge_media_to_comment']
         }
       end
 
